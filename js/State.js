d03d0d5fb32f277e4380f153c679360743b93ed7
Gabe Scholz <gabe@brewhouse.io>
2015-11-07 15:20:17 -0800

    Make sure to shareReplay of the store

--- js/State.js
+++ js/State.js
@@ -5,5 +5,5 @@ function State () {
   }
 
   this.updates = new Rx.BehaviorSubject(Immutable.fromJS(initial))
-  this.asObservable = this.updates.scan((state, operation) => operation(state))
+  this.asObservable = this.updates.scan((state, operation) => operation(state)).shareReplay(1)
 }
