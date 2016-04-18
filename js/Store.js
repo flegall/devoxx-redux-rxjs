4e1e5efa9f9b8d0a2b8348ff25d422d6d739c3af
Florent Le Gall <florent.legall@gmail.com>
2016-04-18 21:08:07 +0200

    Step 0

--- js/Store.js
+++ js/Store.js
@@ -14,11 +14,6 @@ export class Store {
 		}]};
 		this.updates$ = new Rx.BehaviorSubject(Immutable.fromJS(initialState))
 		this.asObservable = this.updates$
-			.scan((state, operation) =>
-				operation(state)
-			)
-			.shareReplay(1)
-		Actions.register(this.updates$)
 	}
 }
 
@@ -26,45 +21,4 @@ export const Actions = {
 	addArticle$: new Rx.Subject(),
 	likeArticle$: new Rx.Subject(),
 	addComment$: new Rx.Subject(),
-
-	register(updates$) {
-		this.addArticle$
-			.map(content => {
-				return state => {
-					const article = Immutable.fromJS({
-							id: uuid.v1(),
-							date: moment().format('LLL'),
-							content,
-							likes: 0,
-							comments: []
-						});
-					return state.update(`articles`, articles => articles.unshift(article))
-				}
-			})
-			.subscribe(updates$)
-
-		this.likeArticle$
-			.map(article => {
-				return state => {
-					return state.update(`articles`, articles => {
-						const likes = article.get('likes') + 1;
-						const index = articles.indexOf(article)
-						return articles.set(index, article.set('likes', likes))
-					})
-				}
-			})
-			.subscribe(updates$)
-
-		this.addComment$
-			.map(({text, article}) => {
-				return state => {
-					return state.update(`articles`, articles => {
-						const comments = article.get('comments').push(text);
-						const index = articles.indexOf(article)
-						return articles.set(index, article.set('comments', comments))
-					})
-				}
-			})
-			.subscribe(updates$)
-	}
 }
